1cff5a56b964f33525d228265148b9c85b2dc9b6
Eric Soroos <eric@soroos.net>
2018-04-03 07:50:01 +0000

    +0.7 Version Bump

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name='django-recurrence',
-    version='1.5.0+eds0.6',
+    version='1.5.0+eds0.7',
     license='BSD',
 
     description='Django utility wrapping dateutil.rrule',
